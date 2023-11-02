d55318a7785e7d7d327f3143582067927ea250d9
Jack Jonathan Haynes <jackie2018@protonmail.com>
2023-11-02 01:36:03 +0100

    updated currency model tests

--- currency_exchange/tests/test_currency_model.py
+++ currency_exchange/tests/test_currency_model.py
@@ -1,74 +1,101 @@
 from django.conf import settings
-from django.test import TestCase
-from django.db.utils import IntegrityError
 from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+from django.test import TestCase
+
 from currency_exchange.models import Currency
 
 
 class CurrencyTestCase(TestCase):
-    def assert_invalid_symbol_not_in_init_currencies(self, invalid_symbol):
-        db_currencies = list(map(
+    def assert_test_code_not_in_db_currency_codes(self, test_code):
+        db_currency_codes = list(map(
             lambda elem: elem[0],
-            Currency.objects.all().values_list("symbol")
+            Currency.objects.all().values_list("code")
         ))
 
         self.assertNotIn(
-            invalid_symbol,
-            db_currencies,
-            f'Symbol "{invalid_symbol}" is not a valid currency symbol, '
-            'remove it from settings.INIT_CURRENCIES and '
-            're-run migration 0002'
+            test_code,
+            db_currency_codes,
+            f'Test currency code "{test_code}" conflicts with settings'
         )
 
     def test_db_contains_default_currencies(self):
-        db_currencies = set(Currency.objects.all().values_list("symbol"))
+        db_currency_codes = set(Currency.objects.all().values_list("code"))
 
-        default_currencies = set(map(
+        default_currency_codes = set(map(
             lambda elem: (elem,),
             getattr(settings, "INIT_CURRENCIES", [])
         ))
 
         self.assertEqual(
-            db_currencies,
-            default_currencies,
+            db_currency_codes,
+            default_currency_codes,
             "Default currencies have not been inserted"
         )
 
     def test_currency_insert(self):
-        symbol = "TEST"
-        self.assert_invalid_symbol_not_in_init_currencies(symbol)
+        code = "TTT"
+        self.assert_test_code_not_in_db_currency_codes(code)
 
-        currency = Currency(symbol=symbol)
+        currency = Currency(code=code)
         currency.save()
         db_currencies = Currency.objects.all()
 
         self.assertIn(currency, db_currencies, "Currency was not inserted")
 
     def test_currencies_are_unique(self):
-        symbol = "DUPLICAT"
-        self.assert_invalid_symbol_not_in_init_currencies(symbol)
+        code = "DUP"
+        self.assert_test_code_not_in_db_currency_codes(code)
 
-        Currency(symbol=symbol).save()
+        Currency(code=code).save()
         with self.assertRaises(
                 IntegrityError,
-                msg="Currency's uniqueness is not enforced"
+                msg="Currency's code uniqueness is not enforced"
         ):
-            Currency(symbol=symbol).save()
+            Currency(code=code).save()
 
-    def test_currency_symbol_max_len(self):
-        too_long_symbol = "TOO_LONG_"
+    def test_currency_code_max_len(self):
+        too_long_code = "LONG"
 
         with self.assertRaises(
                 ValidationError,
-                msg="Currency's length is not enforced"
+                msg="Currency's code max length is not enforced"
         ):
-            Currency(symbol=too_long_symbol).full_clean()
+            Currency(code=too_long_code).full_clean()
+
+    def test_currency_code_min_len(self):
+        too_short_code = "SH"
+
+        with self.assertRaises(
+                ValidationError,
+                msg="Currency's code min length is not enforced"
+        ):
+            Currency(code=too_short_code).full_clean()
 
     def test_currency_symbol_not_null(self):
-        empty_symbol = ""
+        empty_code = ""
 
         with self.assertRaises(
                 ValidationError,
-                msg="Empty symbols shouldn't be allowed"
+                msg="Empty codes shouldn't be allowed"
         ):
-            Currency(symbol=empty_symbol).full_clean()
+            Currency(code=empty_code).full_clean()
+
+    def test_code_valid_characters(self):
+        invalid_code = "H4T"
+
+        with self.assertRaises(
+                ValidationError,
+                msg="Only letters should be allowed in currency codes"
+        ):
+            Currency(code=invalid_code).full_clean()
+
+    def test_to_string(self):
+        code = "TTT"
+        currency = Currency(code=code)
+
+        self.assertEqual(
+            code,
+            str(currency),
+            "Currency's string representation should be equal to it's code"
+        )
